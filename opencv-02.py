cc1f09027d350457abbfeb89bc2db62d1cfb0e6a
Viet281101 <nva281101@gmail.com>
2022-02-14 21:54:58 +0100

    Learning Exercices

---
+++ opencv-02.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+
+imgL = cv.imread('1-Green_320x180.png',0)
+imgR = cv.imread('1-Purple_320x180.png',0)
+
+stereo = cv.StereoBM_create(numDisparities=16, blockSize=15)
+disparity = stereo.compute(imgL,imgR)
+
+plt.imshow(disparity,'gray')
+plt.show()
+
