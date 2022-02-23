db034fcbb1d8ab37df9c65ce6f7fc6ddae745956
Viet281101 <nva281101@gmail.com>
2022-02-24 00:38:15 +0100

    Learning Exercices

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Learning OpenCV in Python
 
 
-### This project is done bit by bit in order of steps on OpenCV Python Doc: https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html
+#### This project is done bit by bit in order of steps on OpenCV Python Doc: 
+
+https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html
 
 
 - The link of the images:
--- opencv-18-blink_counter.py
+++ opencv-18-blink_counter.py
@@ -1,22 +1,36 @@
 
 import cv2
 import cvzone
+from cvzone.FaceMeshModule import FaceMeshDetector
 
-# Use the webcam
+
+### Use the webcam
 cap = cv2.VideoCapture(0)
 
 ### Or you can see the blink in my video here
 # cap = cv2.VideoCapture('webcam_video_blink.mp4')
 
 
+detector = FaceMeshDetector(maxFaces = 1)
+
+
 while True:
+
+	if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+		cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
 	success, img = cap.read()
 
+	img, faces = detector.findFaceMesh(img)
+
+
 	# change the screen size
-	img = cv2.resize(img, (640, 420))
+	img = cv2.resize(img, (640, 480))
+
 
 	# Show
 	cv2.imshow("Window", img)
+
 	# wait in 1/1000s, exit if click q
 	if cv2.waitKey(1) == ord("q"):
 		break
